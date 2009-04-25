7b48f2b920575d7231a41f6fb432663481b8cde4
Martin Fernandez <martin.ariel.fernandez@gmail.com>
2009-04-25 19:18:50 +0000

    - Correcion bug Javasript validacion asincronica de numero de pedido en edit_order.js

--- ofa/src/js/edit_order.js
+++ ofa/src/js/edit_order.js
@@ -6,70 +6,71 @@ function validar () {
 	var pay_term_id = $('pay_term_id').value;
 
 	if ( pedidos <= 0 ) 	msg += "- Por lo menos debe seleccionar un producto.\n";
-	if (term_id == 0) 		msg += "- Debe seleccionar los términos de pago.\n"
+	if (term_id == 0) 	msg += "- Debe seleccionar los términos de pago.\n"
 	if (pay_term_id == 0) 	msg += "- Debe seleccionar la condicion de pago.\n"
 	
-	msg+=validarNumeroPedido();
+	validarNumeroPedido(msg);
 	
-	if ( msg == "") {
-		return true;
-	}
-	else
-	{
-		alert(msg);
-		return false;
-	}
 }
 
 function nuevoAjaxEdit(){
-		var xmlhttp=false;
-		try {
-			xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
-		}
-		catch(e){
-			try{
-				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-			} 
-			catch(E){
-				xmlhttp=false;
-			}
-		}
-		if (!xmlhttp && typeof XMLHttpRequest!='undefined'){xmlhttp=new XMLHttpRequest();} 
-		return xmlhttp; 
+	var xmlhttp=false;
+	try {
+		xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
 	}
+	catch(e){
+		try{
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		} 
+		catch(E){
+			xmlhttp=false;
+		}
+	}
+	if (!xmlhttp && typeof XMLHttpRequest!='undefined'){xmlhttp=new XMLHttpRequest();} 
+	return xmlhttp; 
+}
 	
 
-function validarNumeroPedido(){
-
-	var retorno = "- Ingrese el numero de pedido de formulario (borde superior derecho).\n";
+function validarNumeroPedido(mensaje) {
 	
 	var numero = $('txt_numero_pedido').value;
-	
-	if (!isNaN(Number(numero)) && numero != ''){
-		var ajax = nuevoAjaxEdit();
+
+	mensaje += (!isNaN(Number(numero)) && numero != '')? '' : "- Ingrese el numero de pedido de formulario (borde superior derecho).\n";
+
+	if (mensaje == '')
+	{
+		var obj_ajax = nuevoAjaxEdit();
 		
 		var url = "xml_numero_pedido.php?param=";
 		url += numero;
 		url += "&t=" + new Date().getTime();
 	
-		ajax.open("GET",url, false);
+		obj_ajax.open("GET", url, true);
 		
-		ajax.onreadystatechange = function(){
-			if (ajax.readyState == 4)
-				retorno = (ajax.responseText == 'OK')? '' : "- El número de pedido de formulario ya existe.\n";
+		obj_ajax.onreadystatechange = function(){
+			if (obj_ajax.readyState == 4) {
+				if (obj_ajax.responseText == "OK") 
+				{
+					if (confirm ('Confirma el pedido?') ) 
+					{
+						$('frmEdit').submit();
+					}
+				}
+				else
+				{
+					mensaje += "- El número de pedido de formulario ya existe.\n";
+					alert(mensaje);
+				}
+			}
 		};
-		ajax.send(null);
+		obj_ajax.send(null);
 	}
-	
-	return retorno;
-
+	else 
+	{
+		alert(mensaje);	
+	}
 }
 
 function enviarOrden () {
-	if (!validar()) return;
-	
-	if (confirm ('Confirma el pedido?') ) {
-		$('frmEdit').submit();
-	}
-
+	validar();
 }
